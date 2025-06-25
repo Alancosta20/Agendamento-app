ab3a1a78a2dd24ddf7bd38667954aaa5bbf8e80b
alancosta20 <alancostacs@hotmail.com>
2025-06-25 19:33:13 -0300

    Metodo Edit, Update e destroy

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -17,6 +17,29 @@ class ServicesController < ApplicationController
         end
     end
 
+    def edit
+        @service = Service.find(params[:id])
+    end
+    
+    def update
+        @service = Service.find(params[:id])
+        if @service.update(services_params)
+            redirect_to services_path
+        else
+            render :edit
+        end 
+    end
+
+    def destroy
+        @service = Service.find(params[:id])
+        @service.destroy
+
+        redirect_to services_path
+        
+    end
+    
+    
+
     
 
     private
